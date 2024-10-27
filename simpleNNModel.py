2c952624ced7bfc302abb7680d3197fff1efa08c
Saransh_Kejriwal <saranshkejriwal@gmail.com>
2024-10-27 22:46:04 +0530

    started forward prop code. Matrix dimensions incompatible

--- simpleNNModel.py
+++ simpleNNModel.py
@@ -11,19 +11,59 @@ class simpleNNModel(abstractModel):
     #hyper-parameters
     learning_rate = 0.01
 
+    #Note - 12 aand neurons has been selected somewhat randomly for now, just to kick things off....
+    input_layer_neurons = 12
+    hidden_layer_neurons = 15
+    iterations = 1 #number of iterations for which the model should run
+
     def trainModel(self, x_train, y_train):
         print("training Simple Neural Net Model...")
+
+        W1, b1, W2, b2 = self.initParams()
+
+        for i in range(self.iterations):
+            Z1, A1, Z2, A2 = self.forwardProp(W1, b1, W2, b2, x_train)
+
 
 
 
         return 
 
+    def forwardProp(self, W1, b1, W2, b2, x_train):
+        Z1 = W1.dot(x_train.T) + b1
 
+        print("Z1:" + Z1.shape)
+
+        A1 = self.Tanh(Z1)
+        print("A1:" + A1.shape)
+        
+        Z2 = W2.dot(A1) + b2
+
+        print("Z2:" + Z2.shape)
+
+        A2 = self.softmax(Z2)
+        print("A2:" + A2.shape)
+
+        return Z1, Z1, Z2, A2
+
+
+    #this method initializes the starting weights and biases of the network before training.
     def initParams(self):
 
-        W1 = np.random.randn(12,81) #12 neurons of size 81, to align with input matrix
-        #Note - 12 neurons has been selected somewhat randomly for now, just to kick things off....
+        W1 = np.random.randn(self.input_layer_neurons,81) #12 neurons of size 81, to align with input matrix
+        b1 = np.random.randn(self.input_layer_neurons, 1)
 
+        W2 = np.random.randn(self.hidden_layer_neurons,81) 
+        b2 = np.random.randn(self.hidden_layer_neurons, 1)
 
+        return W1, b1, W2, b2
 
-        return super().initParams()
+    def Tanh(self, z):
+        return np.tanh(z)
+
+    def dTanh(self, tanh):
+        #Derivative of tanh() is 1- tanh()^2
+        return 1 - np.square(tanh)
+
+    def softmax(self,z):
+        return np.exp(z)/np.sum(np.exp(z))
--- sudokuDataManager.py
+++ sudokuDataManager.py
@@ -6,6 +6,7 @@ writes them to static csv files
 and/or loads them from pre-written csv files
 
 '''
+import numpy as np
 
 class sudokuDataManager():
 
@@ -14,7 +15,7 @@ class sudokuDataManager():
     def getSingleInputExample(self):
 
         x_single_input = [0,4,6,0,1,0,8,5,7,0,9,1,0,0,8,2,0,4,2,8,0,5,0,4,1,3,9,1,3,5,2,0,7,0,8,0,7,0,0,6,8,0,4,1,0,4,6,8,1,0,3,0,7,2,0,1,0,3,0,6,0,4,0,6,0,0,8,2,0,3,0,5,0,0,3,0,0,9,6,0,0]
-
+        x_single_input = np.array(x_single_input)
         return x_single_input
 
 
@@ -22,6 +23,6 @@ class sudokuDataManager():
     def getSingleOutputExample(self):
 
         y_single_output = [3,4,6,9,1,2,8,5,7,5,9,1,7,3,8,2,6,4,2,8,7,5,6,4,1,3,9,1,3,5,2,4,7,9,8,6,7,2,9,6,8,5,4,1,3,4,6,8,1,9,3,5,7,2,9,1,2,3,5,6,7,4,8,6,7,4,8,2,1,3,9,5,8,5,3,4,7,9,6,2,1]
-
+        y_single_output = np.array(y_single_output)
         return y_single_output
 
