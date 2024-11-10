corrected loss calculation

mathFunctions.py
@@ -20,7 +20,7 @@ def getGaussianInit(xDim, yDim):
 
 #this takes a digit from 1-9 as an input and returns an array of zeroes such that only the yth value is 1
 def getOneHotVector(y):
-    yOneHot = np.zeros(9,dtype=int)
-    yOneHot[y-1] = 1 #python index starts at 0, hence y-1
+    yOneHot = np.zeros((y.size, 9)) #get m x 9 zeroes which will be arranged into 9 columns, hence an (m,9) vector of zeros
+    yOneHot[np.arange(y.size), y] = 1
 
     return yOneHot

simpleNN.py
@@ -2,6 +2,7 @@
 This class contains a single 10 layer neural network.
 The simpleNNModel will create 81 of these simpleNN objects, one for each cell in Sudoku.
 '''
+from ast import Num
 from math import log
 import mathFunctions
 import numpy as np
@@ -65,19 +66,25 @@ class simpleNN:
 
         #Note - Each neural network applies to an individual cell of the sudoku, so Loss will actually be an 81-dimension array
 
+        numExamples = np.shape(y_train)[0] #get number of rows in y_train
+
+        print(numExamples)
+
         yOneHot = mathFunctions.getOneHotVector(y_train)
 
         #get the indices from the predictions, corresponding to the outputs y of interest.
-        print(y_train)
-        print(A2)
-        print(yOneHot)
-        #probabilityOfExpectedOutput = A2[range(yOneHot.size),yOneHot]
-        probabilityOfExpectedOutput = np.multiply(A2,yOneHot) #get the probability of ONLY the expected output via element-wise multiplication
+        probabilityOfExpectedOutput = np.multiply(A2,yOneHot.T) #get the probability of ONLY the expected output via element-wise multiplication
 
-        print(probabilityOfExpectedOutput) #test only
 
         #using negative log likelihood method to calculate loss value for all the training examples
-        self.currentLoss = (-1 * sum(log(probabilityOfExpectedOutput)))/probabilityOfExpectedOutput.size
+
+        logA2 = -1 * np.log(A2)
+
+        #self.currentLoss = (-1 * np.sum(np.log(probabilityOfExpectedOutput)))
+        #Note - Do NOT attempt an element wise multiplication and take a log of that, because most elements there will be 0, and log(0) is -infinity
+        self.currentLoss = np.multiply(logA2,yOneHot.T).sum()/numExamples #get the loss against the indices of the expected output value.        
+
+        #print("current loss: ",self.currentLoss)
 
         return probabilityOfExpectedOutput
 

simpleNNModel.py
@@ -25,13 +25,21 @@ class simpleNNModel(abstractModel):
     def trainModel(self, x_train, y_train):
         print("training Simple Neural Net Model with 81 neural nets for each cell...")
 
-        y_train_formatted = np.reshape(y_train, (-1, self.sudokuGridSize)) #convert a list of 81 numbers into a 9x9 2D vector
+        print(y_train)
+        print (y_train[:,0])
 
         for t in range(self.iterations):
 
             for i in range (self.sudokuGridSize):
                 for j in range (self.sudokuGridSize):
-                    self.sudokuSimpleNN[i][j].trainModel(x_train, y_train_formatted[i][j]) #each network will receive the entire grid of input sudoku but will focus only on its own cell.
+
+                    '''From an array of [m,81], where m is the training example, we need to get the output for each cell individually, since one NN corresponds to one cell. 
+
+                        if you distribute a row of 81 entries on a 9x9 grid, cell (i,j) will contain entry number 9i+j '''
+                    y_train_cell_ij = y_train[:,9*i + j] 
+                    
+
+                    self.sudokuSimpleNN[i][j].trainModel(x_train, y_train_cell_ij) #each network will receive the entire grid of input sudoku but will focus only on its own cell.
 
         return 
 