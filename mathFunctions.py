critical bugfix in softmax function

mathFunctions.py
@@ -11,7 +11,9 @@ def dTanh(tanh):
     return 1 - np.square(tanh)
 
 def softmax(z):
-    return np.exp(z)/(np.sum(np.exp(z))) 
+    A2 = np.exp(z)/(np.sum(np.exp(z),axis = 0)) 
+    #axis=0 argument is important, to ensure that the sum is only along the preactivations of ONE training example, not against the ENTIRE dataset. 
+    return A2
 
 #this function returns a list, or list of lists, of random values between -0.5 and 0.5
 #xDim is the size of the outer list and yDim is the siz of the inner list.

simpleNN.py
@@ -94,7 +94,9 @@ class simpleNN:
 
         #get the indices from the predictions, corresponding to the outputs y of interest.
         probabilityOfExpectedOutput = np.multiply(A2,yOneHot.T) #get the probability of ONLY the expected output via element-wise multiplication
-
+        
+        #print(A2)
+        #print(probabilityOfExpectedOutput)
 
         #using negative log likelihood method to calculate loss value for all the training examples
         try:

simpleNNModel.py
@@ -29,7 +29,7 @@ class simpleNNModel(abstractModel):
 
             if(t%50==0):
                 print("Now starting Iteration ",t)
-                print("Model loss by cell: ", self.modelLoss)
+                #print("Model loss by cell: ", self.modelLoss)
                 print("Average model loss: ", np.mean(self.modelLoss))
 
             for i in range (self.sudokuGridSize):